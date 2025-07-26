769f21758a3a35c3c92a51b8c00a697997e80fbb
Yuriy Shkitskiy <skyranger@MacBook-Pro-Yuriy.local>
2025-07-26 11:11:41 +0300

    delete grid_size2

--- the_snake.py
+++ the_snake.py
@@ -5,7 +5,6 @@ import pygame
 # Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
-GRID_SIZE2 = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
