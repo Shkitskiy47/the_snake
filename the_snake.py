c8a5ea74da354dc663e865d0aff26f1b9c905d27
skyranger <shkitskiy94@gmail.com>
2026-03-22 15:50:28 +0300

    test

--- the_snake.py
+++ the_snake.py
@@ -1,7 +1,8 @@
 import pygame
+
 from random import randint, choice
 
-# Константы для размеров поля и сетки:
+# Константы для размеров поля и сетки
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
